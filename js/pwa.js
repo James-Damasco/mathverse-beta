3fc775d296eeabf3d8ef73b4b40ffa4086a122e3
James Damasco <damascojames488@gmail.com>
2026-06-08 23:31:10 +0800

    Add files via upload

---
+++ js/pwa.js
@@ -0,0 +1,10 @@
+if ("serviceworker" in navigator) {
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("./sw.js").then(registration => {
+            console.log("Service Worker registered successfully: ", registration.scope);
+        })
+            .catch(error => {
+                console.error("Service Worker registration failed: ", error);
+            });
+    });
+}
